2f3aebf6dc2e14988d7e3738d589ae83d74ed422
simplilearn github <aman.ap2@gmail.com>
2023-11-10 08:54:53 +0530

    final

--- src/components/MainContent/HeroSection/Timer.js
+++ src/components/MainContent/HeroSection/Timer.js
@@ -1,64 +1,57 @@
 import React, { useEffect, useState } from 'react';
-import './Timer.css'
+import './Timer.css';
 
 function Timer() {
-    const [days, setDays] = useState(0);
-    const [hours, setHours] = useState(0);
-    const [minutes, setMinutes] = useState(0);
-    const [seconds, setSeconds] = useState(0);
+    const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
 
     useEffect(() => {
-        const second = 1000,
-            minute = second * 60,
-            hour = minute * 60,
-            day = hour * 24;
-
-        let today = new Date();
-        let dd = String(today.getDate()).padStart(2, "0");
-        let mm = String(today.getMonth() + 1).padStart(2, "0");
-        let yyyy = today.getFullYear();
-        let nextYear = yyyy + 1;
-        let dayMonth = "09/30/";
-        let birthday = dayMonth + yyyy;
-
-        today = mm + "/" + dd + "/" + yyyy;
-        if (today > birthday) {
-            birthday = dayMonth + nextYear;
-        }
-
-        const countDown = new Date(birthday).getTime();
-        const interval = setInterval(() => {
-            const now = new Date().getTime();
-            const distance = countDown - now;
-
-            setDays(Math.floor(distance / day));
-            setHours(Math.floor((distance % day) / hour));
-            setMinutes(Math.floor((distance % hour) / minute));
-            setSeconds(Math.floor((distance % minute) / second));
-
-            if (distance < 0) {
-                clearInterval(interval);
-            }
+        const timer = setInterval(() => {
+            setTimeLeft(calculateTimeLeft());
         }, 1000);
 
         return () => {
-            clearInterval(interval);
+            clearInterval(timer);
         };
     }, []);
 
+    function calculateTimeLeft() {
+        const targetDate = new Date('2023-12-30');
+        const now = new Date();
+        const timeRemaining = targetDate - now;
+
+        if (timeRemaining <= 0) {
+            // If the target date has passed, calculate time remaining from the current date
+            const newTargetDate = new Date(now.getTime() + 24 * 60 * 60 * 1000); // Set the target date to the next day
+            return calculateTimeLeft(newTargetDate);
+        }
+
+        const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+
+        return { days, hours, minutes, seconds };
+    }
+
     return (
-        <>
-            <div className="container" style={{ color: 'white' }}>
-                <div id="countdown">
-                    <ul>
-                        <li className='list1'><span id="days">{days}</span>DAYS</li>
-                        <li className='list1'><span id="hours">{hours}</span>HOURS</li>
-                        <li className='list2'><span id="minutes">{minutes}</span>MINUTES</li>
-                        <li className='list2'><span id="seconds">{seconds} </span>SECONDS</li>
-                    </ul>
-                </div>
+        <div style={{ color: 'white' }}>
+            <div id="countdown">
+                <ul>
+                    <li className='list1' style={{ marginRight: '46px' }}>
+                        <span id="days" style={{ textAlign: 'center' }}>{timeLeft.days}</span>
+                        <span className='timerText'>DAYS</span></li>
+                    <li className='list1' style={{ marginRight: '46px' }}>
+                        <span id="hours" style={{ textAlign: 'center' }}>{timeLeft.hours} </span>
+                        <span className='timerText' style={{ marginLeft: '32px' }}>HOURS</span></li>
+                    <li className='list2' style={{ marginRight: '46px' }}>
+                        <span id="minutes" style={{ textAlign: 'center' }}>{timeLeft.minutes} </span>
+                        <span className='timerText' style={{ marginLeft: '27px' }}>MINUTES</span></li>
+                    <li className='list2'>
+                        <span id="seconds" style={{ textAlign: 'center' }}>{timeLeft.seconds} </span>
+                        <span className='timerText' style={{ marginLeft: '25px' }}>SECONDS</span></li>
+                </ul>
             </div>
-        </>
+        </div>
     );
 }
 
